0635f1b9588fa5a6d77ad7e71ceb641d35fb6322
Dustin Younse <milsyobtaf@gmail.com>
2013-01-08 16:26:11 -0600

    added some header structure, path variable still not functioning

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -73,27 +73,9 @@
 <div id="page">
 
   <header id="header" role="banner">
-    <a href="http://www.utexas.edu" title="University of Texas at Austin Home Page" id="universitylogo"><img src="<?php $path ?>images/tower-header.jpg" /></a>
-    <?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
-    <?php endif; ?>
-    <span><img src="<?php $path; ?>images/slogan.png" /></span>
-
-    <?php if ($site_name || $site_slogan): ?>
-      <hgroup id="name-and-slogan">
-        <?php if ($site_name): ?>
-          <h1 id="site-name">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-          </h1>
-        <?php endif; ?>
-
-        <?php if ($site_slogan): ?>
-          <h2 id="site-slogan"><?php print $site_slogan; ?></h2>
-        <?php endif; ?>
-      </hgroup><!-- /#name-and-slogan -->
-    <?php endif; ?>
-
-    <?php if ($secondary_menu): ?>
+    <span id="header-top">
+      <a href="http://www.utexas.edu" title="University of Texas at Austin Home Page" class="universitylogo"><img src="<?php $path ?>images/tower-header.jpg" />&nbsp;</a>
+      <?php if ($secondary_menu): ?>
       <nav id="secondary-menu" role="navigation">
         <?php print theme('links__system_secondary_menu', array(
           'links' => $secondary_menu,
@@ -108,6 +90,17 @@
         )); ?>
       </nav>
     <?php endif; ?>
+    </span>
+    <span id="header-mid">
+    <?php if ($logo): ?>
+      <span id="site-logo">
+        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
+      </span>
+    <?php endif; ?>
+      <span id="site-slogan"><img src="<?php $path; ?>images/slogan.png" />&nbsp;</span>
+      <span id="site-search">
+        Search Box Here
+      </span>
 
     <?php print render($page['header']); ?>
 
